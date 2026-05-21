425400c0a5614adb834b94be10eb93c5845134c9
premkathayatpk <premkathayat13@gmail.com>
2026-05-21 14:34:26 +0545

    creating delete, update api for product

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Product from "../models/productModel.js";
 
+// Create new product
 export const createProduct = async (req, res) => {
   try {
     const { name, price, description, category, stock } = req.body;
@@ -28,6 +29,7 @@ export const createProduct = async (req, res) => {
   }
 };
 
+// Fetching all product
 export const getAllProduct = async (req, res) => {
   try {
     const products = await Product.find();
@@ -41,6 +43,118 @@ export const getAllProduct = async (req, res) => {
     res.status(500).json({
       status: "Error ",
       message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+// Fetching product by id
+export const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({
+        status: "fail",
+        message: "No product found with that ID",
+      });
+    }
+
+    res.status(200).json({
+      status: "success ",
+      message: "Fetched  product successfully",
+      product,
+    });
+  } catch (error) {
+    console.error("Error fetching  product:", error);
+    res.status(500).json({
+      status: "Error ",
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+// update product by id
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, description, category, stock } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        name,
+        price,
+        description,
+        category,
+        stock,
+      },
+      { returnDocument: "after", runValidators: true },
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({
+        status: "fail",
+        message: "No product found with that ID",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Product update successfully",
+      updatedProduct,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res
+      .status(500)
+      .json({ status: "error", message: "Server Error", error: error.message });
+  }
+};
+
+// Delete product by id
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      res
+        .status(404)
+        .json({ status: "fail", message: "No Product found with that ID" });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Product deleted successfully.",
+      deletedProduct,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({
+      status: "error",
+      message: "Fail to delete product",
+      error: error.message,
+    });
+  }
+};
+
+// Delete all product
+export const deleteAllProducts = async (req, res) => {
+  try {
+    const result = await Product.deleteMany({});
+
+    res.status(200).json({
+      status: "success",
+      message: "All Product deleted successfully.",
+      count: result.deletedCount,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({
+      status: "error",
+      message: "Fail to delete All product",
       error: error.message,
     });
   }
--- server/src/routers/productRouter.js
+++ server/src/routers/productRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
 import {
   createProduct,
+  deleteAllProducts,
+  deleteProduct,
   getAllProduct,
+  getProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 
 export const productRouter = express.Router();
 
 productRouter.post("/create", createProduct);
-productRouter.get("/getALl", getAllProduct);
+productRouter.get("/all", getAllProduct);
+productRouter.get("/find/:id", getProduct);
+productRouter.put("/update/:id", updateProduct);
+productRouter.delete("/delete/:id", deleteProduct);
+productRouter.delete("/deleteAll", deleteAllProducts);
