8b36ae1a385d01a75e9ef3011d0a509cd78814a3
premkathayatpk <premkathayat13@gmail.com>
2026-06-02 22:38:49 +0545

    update addproduct and connect with api

--- Frontend/src/layouts/AdminLayout.jsx
+++ Frontend/src/layouts/AdminLayout.jsx
@@ -5,11 +5,15 @@ import AdminNavbar from "../components/admin/AdminNavbar";
 
 const AdminLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col h-screen w-full bg-gray-50 overflow-hidden">
       <AdminNavbar />
-      <div className="flex ">
-        <Sidebar />
-        <main>
+
+      <div className="flex flex-1 w-full overflow-hidden">
+        <aside className=" bg-white border-r border-gray-200 h-full  ">
+          <Sidebar />
+        </aside>
+
+        <main className=" w-4/5 p-6 h-full overflow-y-auto">
           <Outlet />
         </main>
       </div>
--- Frontend/src/pages/admin/AdminProducts.jsx
+++ Frontend/src/pages/admin/AdminProducts.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddProduct from "./AddProduct";
 
 const AdminProducts = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>
-      <AddProduct />
+    <div className="p-6  w-[100%]">
+      <div className="w-full flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold text-gray-800">Products Inventory</h1>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="cursor-pointer bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg font-medium shadow-sm transition-colors"
+        >
+          + Add Product
+        </button>
+      </div>
+
+      {isModalOpen && <AddProduct onClose={() => setIsModalOpen(false)} />}
     </div>
   );
 };
